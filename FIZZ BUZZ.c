4c1b62391b1bebf0a1f2ae32e67a8ef31af404b0
AbhishekLwagun <69157459+AbhishekLwagun@users.noreply.github.com>
2020-10-05 19:43:04 +0545

    Update and rename FIZZ BUZZ.cpp to FIZZ BUZZ.c

---
+++ FIZZ BUZZ.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+   main()
+   {
+   	int j;
+   	for(j=1;j<=100;j++){
+	   
+	
+   if((i % (3*5)) == 0)
+{
+printf("FizzBuzz\n");
+}
+else if ((i % 5) == 0)
+{
+printf("Buzz\n");
+}
+else if ((i % 3) == 0)
+{
+printf("Fizz\n");
+}
+else
+{
+printf("%d\n",i);
+}	  
+   }
+getch();
+ }
