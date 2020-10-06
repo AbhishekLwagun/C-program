bbc5f8c0e123342226a748fa02c64b4127823685
Abhishek <69157459+AbhishekLwagun@users.noreply.github.com>
2020-10-06 08:56:30 +0545

    Add files via upload

---
+++ area.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+    int main(){
+        float r, pi=3.142, area;
+        printf("enter the radius of a circle:\n");
+        scanf("%f", &r);
+        area = pi*r*r;
+        printf("the area of a circle is %f\n", area);
+    return 0;
+}1--
---
+++ ifstate.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+    int main(){
+        int n = 9;
+        if(n==1){
+        printf("hello this is admin");
+        }else if(n==2){
+         printf("hello tthis is user");
+        }else{
+            printf("hello this value is unexpected");
+        }
+    return 0;
+}
