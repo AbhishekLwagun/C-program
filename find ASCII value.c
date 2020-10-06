1a1e91f32a8b131f45876e179f03c2f71507f390
Abhishek <69157459+AbhishekLwagun@users.noreply.github.com>
2020-10-06 21:26:06 +0545

    Create find ASCII value.c
    find ASCII value of a given character.

---
+++ find ASCII value.c
@@ -0,0 +1,19 @@
+//Below is a program to find ASCII value of any input character.
+
+//%c is the format specifier to take character as input
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+    char c;
+    printf("Enter a character : ");
+    scanf("%c" , &c);
+    printf("\n\nASCII value of %c = %d",c,c);
+    return 0;
+    
+    //coding is fun 
+    //enjoy
+}
