eedbae310c98a8c25da6d5146f8fbbd1988e9d5f
Abhishek <69157459+AbhishekLwagun@users.noreply.github.com>
2020-10-06 21:10:16 +0545

    Create Check vowel letter.c

---
+++ Check vowel letter.c
@@ -0,0 +1,32 @@
+//Program to check if input character is a vowel using Switch Case.
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+    char ch;
+    printf("Input a Character :  ");
+    scanf("%c", &ch);
+
+    switch(ch)
+    {
+        case 'a':
+        case 'A':
+        case 'e':
+        case 'E':
+        case 'i':
+        case 'I':
+        case 'o':
+        case 'O':
+        case 'u':
+        case 'U':
+            printf("\n\n%c is a vowel.\n\n", ch);
+            break;
+        default:
+            printf("%c is not a vowel.\n\n", ch);
+    }
+    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+    return 0;
+}
