0c542a4027e4870d926b25baee420ee703ae10dd
Abhishek <69157459+AbhishekLwagun@users.noreply.github.com>
2020-10-08 19:26:01 +0545

    Add files via upload

---
+++ GPA.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+   main()
+   {
+   	float gpa , per;
+   	printf("\nEnter your GPA: ");
+   	scanf("%f", &gpa);
+   	per=gpa/4*100;
+   	printf("Percentage is : %f",per);
+   	getch();
+   	
+   }
---
+++ per.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+   int main(){
+   	
+   	float a, b , c , d, e, t, Av;
+   	 
+   	 printf("enter your marks");
+   	 scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+   	 
+   	 t = a+b+c+d+e;
+   	 printf("totoal is %d",t);
+   	 
+   	 Av= t/5;
+   	 printf("average is%d",a);
+   	 
+   	 
+   	 
+
+   }
