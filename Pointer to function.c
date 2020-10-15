e128fbf2bff2358f140a8da74361ac6660227a50
Abhishek <69157459+AbhishekLwagun@users.noreply.github.com>
2020-10-15 08:47:15 +0545

    Create Pointer to function.c

---
+++ Pointer to function.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+int func(int a, int b)  // function definition
+{
+    printf("\n\n a = %d \n", a);
+    printf("\n\n b = %d \n", b);
+}
+
+int main()
+{
+    printf("\n\n\t\tStudy with - Abhishek\n\n\n");
+
+    // function pointer
+    int(*fptr)(int , int);
+
+    // assign address to function pointer
+    fptr = func;
+
+    // function calling
+    func(2,3);
+    fptr(2,3);  // calling a function referring to pointer to a function
+
+    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+    return 0;
+}
+
+
