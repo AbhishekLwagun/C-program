956f1bc813c5d95111715830bdb1d5853d2ff730
Abhishek <69157459+AbhishekLwagun@users.noreply.github.com>
2020-11-01 09:32:49 +0545

    Update Palindrome using recursio.c

--- Palindrome using recursio.c
+++ Palindrome using recursio.c
@@ -4,3 +4,31 @@
 
 #include<stdio.h>
 #include<conio.h>
+
+
+// declaring the recursive function
+int isPal(int );
+
+/*
+    global declaration to use the same value 
+    in both the functions
+*/
+int n;
+
+int main()
+{
+    printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
+    int palindrome;
+    printf("\n\nEnter a number to check for Palindrome: ");
+    scanf("%d", &n);
+    palindrome = isPal(n);
+    if(palindrome == 1)
+        printf("\n\n\n%d is palindrome\n\n", n);
+    else
+        printf("\n\n\n%d is not palindrome\n\n", n);
+
+    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+    return 0;
+}
+
+
