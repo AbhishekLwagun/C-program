a7ded2e183d7f1084e828514458cbbf81bcd7c6d
Abhishek <69157459+AbhishekLwagun@users.noreply.github.com>
2020-11-01 09:31:27 +0545

    Update Palindrome using recursio.c

--- Palindrome using recursio.c
+++ Palindrome using recursio.c
@@ -1,1 +1,4 @@
+//A Palindrome is a sequence that if reversed looks identical to the original sequence Eg : abba, level, 999 etc.
+
+//Below is a simple C program to find whether the user input number is a palindrome or not using recursion:
 
