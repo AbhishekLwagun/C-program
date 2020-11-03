cee40cd862096d0962ff37318c30dda66ababf8d
Abhishek <69157459+AbhishekLwagun@users.noreply.github.com>
2020-11-03 20:28:53 +0545

    Update sum of series x + x^2.c

--- sum of series x + x^2.c
+++ sum of series x + x^2.c
@@ -8,14 +8,14 @@ int i,n;
 float x,sum=0;
 clrscr(); //to clear the screen
     printf("\n\n\t\tStudy with Abhishek\n\n\n");
-printf(“x+x^2/2+x^3/3+…..+x^n/n”);
-printf(“nEnter value of x and n:”);
-scanf(“%f%d”,&x,&n);
+printf("x+x^2/2+x^3/3+…..+x^n/n");
+printf("/nEnter value of x and n:");
+scanf("%f%d",&x,&n);
 
 for(i=1;i<=n;++i)
 sum+=pow(x,i)/i;
 
-printf(“nsum=%f”,sum);
+printf("nsum=%f",sum);
 
  printf("\n\n Coding is Fun..... ");
 getch(); //to stop the screen
