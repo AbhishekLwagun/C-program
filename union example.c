cc912680ab453eebfd821e333fdeec3161eb9eea
Abhishek <69157459+AbhishekLwagun@users.noreply.github.com>
2020-11-07 19:37:53 +0545

    Create union example.c

---
+++ union example.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+#include<conio.h>
+  
+// Declaration of union is same as structures 
+union test { 
+    int x, y; 
+}; 
+  
+int main() 
+{ 
+    // A union variable t 
+    union test t; 
+  
+    t.x = 2; // t.y also gets value 2 
+    printf("After making x = 2:\n x = %d, y = %d\n\n", 
+           t.x, t.y); 
+  
+    t.y = 10; // t.x is also updated to 10 
+    printf("After making y = 10:\n x = %d, y = %d\n\n", 
+           t.x, t.y); 
+    return 0; 
+b 
