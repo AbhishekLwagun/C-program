d46bd6e928a1b0fc465d3dafec1f9e01c019db0f
Abhishek <69157459+AbhishekLwagun@users.noreply.github.com>
2020-11-15 11:41:44 +0545

    Update StringUppperCase.c

--- StringUppperCase.c
+++ StringUppperCase.c
@@ -1,1 +1,15 @@
-Helllo  world
+#include <stdio.h>
+#include <string.h>
+// study with - Abhishek
+int main(){
+
+char firstName[25];
+
+
+printf("Enter first name:\n");
+gets(firstName);
+
+printf("Upper First name is %s",strupr(firstName));
+// Coding is Fun.
+return 0;
+}
