1a6a3d2a2a0a999f2b426aee93fb67e23c461224
Abhishek <69157459+AbhishekLwagun@users.noreply.github.com>
2020-11-16 07:47:56 +0545

    Create StringCopy.c

---
+++ StringCopy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+// Study with - Abhishek
+int main(){
+	// strcpy is used to copy string
+	char a[100] = "Hari";
+	char b[100];
+	
+	strcpy(b,a);
+	printf("%s",b);
+	// Coding is fun
+	return 0;
+}
