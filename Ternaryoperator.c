512fde906656f0a3c1245e7d7dfc9b88cc8a2002
Abhishek <69157459+AbhishekLwagun@users.noreply.github.com>
2020-11-21 07:41:54 +0545

    Create Ternaryoperator.c

---
+++ Ternaryoperator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+// Study with - Abhishek
+int main(){
+	int age;
+	printf("Enter You Age:");
+	scanf("%d",&age);
+	
+	age>22 ? printf("You are voter.") : printf("You are not voter."); 
+	// Coding is fun.
+	return 0;
+}
