2ce6a0d361e9fdfae4e113dc8aa263079c47afad
Abhishek <69157459+AbhishekLwagun@users.noreply.github.com>
2020-11-22 07:31:13 +0545

    Create dowhileloop.c

---
+++ dowhileloop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+// Study with - Abhishek
+
+int main(){
+	int i=12;
+	
+	do{
+		printf("%d\n",i);
+		i++;
+	}while(i<=10);
+		// Coding is fun.
+	return 0;
+}
